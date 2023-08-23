19888b64615d2ce400b68ead0f6fe577f162ea0c
Patrick Vassell <Pvass24@gmail.com>
2023-08-23 19:31:25 -0400

    Update helloAWS.groovy

--- vars/helloAWS.groovy
+++ vars/helloAWS.groovy
@@ -3,3 +3,14 @@ def call(String region){
          sh 'aws s3 ls'
     }
 }
+
+
+myRole = "valid AWS role name" <- last part of valid AWS Arn
+myRegion = "valid AWS region" <- useful for other AWS functions but not needed for role assumption
+myRoleAccount = "valid AWS account number"
+myExternalId = "if role has externalId set, include it here"
+myDuration = 3600 <- limit to 1 hour
+
+WithAWS(role: myRole, region: myRegion, roleAccount: myRoleAccount, externalId: myExternalId, duration: myDuration ){
+  some groovy code
+}
