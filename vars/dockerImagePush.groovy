a5782dbdc1ca9a04e6376489ba4c60bcc5dbf33f
Patrick Vassell <Pvass24@gmail.com>
2023-08-23 16:53:25 -0400

    Update dockerImagePush.groovy

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -19,10 +19,15 @@
 //    """
 // }
 
-def call(String aws_account_id, String region, String ecr_repoName){
-    sh """
-     AWS_ECR_LOGIN=\$(aws ecr get-login-password --region ${region})
-     \${AWS_ECR_LOGIN}  # Execute the AWS ECR login command
-     docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
-    """
+def call(String aws_account_id, String region, String ecr_repoName) {
+    def awsEcrLoginCommand = "aws ecr get-login-password --region ${region}"
+    def dockerLoginCommand = "docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com"
+
+    def dockerPushCommand = "docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest"
+
+    def awsEcrLoginOutput = sh(script: awsEcrLoginCommand, returnStdout: true).trim()
+    sh(script: "echo '${awsEcrLoginOutput}' | ${dockerLoginCommand}", returnStatus: true)
+
+    sh(script: dockerPushCommand, returnStatus: true)
 }
+
