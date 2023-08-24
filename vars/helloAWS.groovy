67d73396baf163c0990eca61fc22e3c72980c064
Patrick Vassell <Pvass24@gmail.com>
2023-08-23 20:45:49 -0400

    Update helloAWS.groovy

--- vars/helloAWS.groovy
+++ vars/helloAWS.groovy
@@ -1,5 +1,12 @@
- def call(String myRole, String region, String aws_account_id, Integer myDuration) {
-    withAWS(role: myRole, 'region': region, roleAccount: aws_account_id, duration: myDuration) {
+// def call(String myRole, String region, String aws_account_id, Integer myDuration) {
+//    withAWS(role: myRole, 'region': region, roleAccount: aws_account_id, duration: myDuration) {
+//         sh 'aws s3 ls'
+//    }
+//}
+
+
+ def call(String region) {
+    withAWS('region': region) {
          sh 'aws s3 ls'
     }
 }
