cb505cab81754016e9d48887fc12268a7232ec2f
Patrick Vassell <Pvass24@gmail.com>
2023-08-23 20:35:59 -0400

    Update helloAWS.groovy

--- vars/helloAWS.groovy
+++ vars/helloAWS.groovy
@@ -1,5 +1,5 @@
  def call(String myRole, String region, String aws_account_id, Integer myDuration) {
     withAWS(role: myRole, 'region': region, roleAccount: aws_account_id, duration: myDuration) {
-         sh 'aws s3 ls'
+         sh 'sudo yum install awscli -y'
     }
 }
